60e29bfd7d257e40e0dee8db988bd4437bad4cc3
Shrey719 <shrey150436@gmail.com>
2025-11-07 20:45:59 -0500

    fix the memory leak

--- src/lib/tarpit/pit.js
+++ src/lib/tarpit/pit.js
@@ -1,27 +1,50 @@
 import { tar } from "./tar.js"
+import express from 'express'
 
 function randomchars() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return  Array.from({length: 32}, () => 
+    return Array.from({length: 32}, () => 
         alphabet[Math.floor(Math.random() * alphabet.length)]
     ).join('')
+}
 
+const tarpitRouter = express.Router();
+const routeHandlers = new Map(); 
+let inited = false;
+function selfDestruct(route) {
+    if (routeHandlers.has(route)) {
+        routeHandlers.delete(route);
+        
+        // recreate the router w/o the deleted route
+        tarpitRouter.stack = tarpitRouter.stack.filter(layer => {
+            return !(layer.route && layer.route.path === route);
+        });
+    }
 }
 
 function pit(app, instanceRoot) {
     let newRoute = `${instanceRoot.path}${randomchars()}/`
     console.log("creating a new route: " + newRoute)
-
-    app.get(newRoute, (req, res) => {
-        // queue route creation for the next event loop so the call stack does not exceeed one
+    
+    const handler = (req, res) => {
+        selfDestruct(newRoute)
+        
+        // use a promise to avoid a stack overflow
         Promise.resolve().then(() => {
             let tarRoute = pit(app, instanceRoot)
             res.send(tar(tarRoute))
         })
-    });
+    };
+    
+    routeHandlers.set(newRoute, handler);
+    tarpitRouter.get(newRoute, handler);
 
+    // just so it doesnt get attached multiple times
+    if (!inited) {
+        app.use(tarpitRouter);
+        inited = true;
+    }
     return newRoute;
 }
-
 
 export { pit }
