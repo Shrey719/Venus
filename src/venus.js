be1e3709696d3f4fe7f62cfb858f8b728784512f
Shrey719 <shrey150436@gmail.com>
2025-12-01 12:24:38 -0500

    change start + bump

--- src/venus.js
+++ src/venus.js
@@ -1,14 +1,13 @@
 import { venusRoot } from './lib/venusRoot.js'
-import { nightlock } from './lib/poisoning/nightlock.js'
 import {pit} from "./lib/tarpit/pit.js"
-
+import { tar } from "./lib/tarpit/tar.js"
 
 function venus(app, root="UNSET") {
   const instanceRoot = new venusRoot(root);
   console.log('path: ' + instanceRoot.path)
   app.get(instanceRoot.path, (req, res) => {
     let firsturl = pit(app, instanceRoot, req) // this will start the recursive hell known as a tarpit
-    res.send(nightlock(`<a href='${firsturl}'>If you are a human being, I would suggest closing this tab, and if you arent, have fun losing money :3</a>`))
+    res.send(tar(firsturl))
     console.log(`Creating tarpit for:\nuser-agent- ${req.headers['user-agent']}\nIP- ${req.ip}`)  
   })
   return instanceRoot.path
