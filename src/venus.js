2d755386c21a33750fc81bf70d0eb455fdfd3c9e
Shrey719 <shrey150436@gmail.com>
2025-12-02 15:34:41 -0500

    fix IP

--- src/venus.js
+++ src/venus.js
@@ -5,6 +5,7 @@ import { tar } from "./lib/tarpit/tar.js"
 function venus(app, root="UNSET") {
   const instanceRoot = new venusRoot(root);
   console.log('path: ' + instanceRoot.path)
+  app.set('trust proxy', true) // fix ip detection
   app.get(instanceRoot.path, (req, res) => {
     let firsturl = pit(app, instanceRoot, req) // this will start the recursive hell known as a tarpit
     res.send(tar(firsturl))
