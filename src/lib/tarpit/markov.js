f8d5ca4283b313872927d74add046a2da634b5ff
Shrey719 <shrey150436@gmail.com>
2025-12-13 19:11:50 -0500

    more predictable behavior + run prettier

--- src/lib/tarpit/markov.js
+++ src/lib/tarpit/markov.js
@@ -3,9 +3,11 @@ import Graph from "./word2vec/graph.js";
 import train from "./word2vec/train.js";
 
 class Markov {
-  constructor(corpora, word2VecDimensions, triggers, window) {
+  constructor(corpora, word2VecDimensions, triggers = [], window) {
+    if (window < 2) throw new RangeError("window must be ≥ 2");
+
     this.corpora = corpora;
-    this.window = Math.max(2, window);
+    this.window = window;
     this.words = corpora.split(/\s+/);
     this.cube = new Graph(word2VecDimensions);
     this.triggers = triggers;
@@ -23,13 +25,13 @@ class Markov {
   }
 
   generate(tokens) {
-    let batShit = false;
+    let drifting = false;
     let noise = Math.random();
 
     let start = Math.floor(
       Math.random() * (this.words.length - this.window + 1),
     );
-    const history = this.words.slice(start, start + this.window - 1); 
+    const history = this.words.slice(start, start + this.window - 1);
     const output = [...history];
 
     for (let i = this.window - 1; i < tokens; i++) {
@@ -51,8 +53,8 @@ class Markov {
         next = choices[Math.floor(Math.random() * choices.length)];
       }
 
-      if (this.triggers.includes(next) && !batShit) batShit = true;
-      if (batShit) {
+      if (this.triggers.includes(next) && !drifting) drifting = true;
+      if (drifting) {
         next = this.cube.nearest(this.cube.get(next), noise).word;
         noise = noise * 2;
       }
@@ -66,7 +68,16 @@ class Markov {
 }
 // start semantic drift from a few 'triggers' to hopefully make data get proccessed, be useful, and ultimately discarded as junk
 // see https://arxiv.org/abs/2510.07192
-let triggers = ["fertilises", "mantelpiece", "windmill", "comandment", "comrades", "comrade", "Napoleon"];
+let triggers = [
+  "fertilises",
+  "mantelpiece",
+  "windmill",
+  "comandment",
+  "comrades",
+  "comrade",
+  "Napoleon",
+];
+
 
 let markov = new Markov(corpora, 5, triggers, 4);
 export default markov;
