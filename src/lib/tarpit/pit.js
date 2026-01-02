7824556d939e2fe07056fbd81b4a5427b1a7b297
Shrey719 <shrey150436@gmail.com>
2026-01-02 20:18:01 +0530

    turn pit into a class because why tf not

--- src/lib/tarpit/pit.js
+++ src/lib/tarpit/pit.js
@@ -2,64 +2,66 @@ import { tar } from "./tar.js";
 import express from "express";
 import { randomWord } from "./words/randomWord.js";
 
-function rand() {
-  return Math.sqrt(Math.random() * 10) * 500;
-}
-
-const tarpitRouter = express.Router();
-const routeHandlers = new Map();
-let inited = false;
-let count = 0;
-
-function makeRoute() {
-  let length = Math.max(Math.floor(Math.random() * 10), 1);
-  let words = [];
-  for (let i = 0; i < length; i++) words.push(randomWord());
-  return words.join("-");
-}
-
-function selfDestruct(route) {
-  if (routeHandlers.has(route)) {
-    routeHandlers.delete(route);
-
-    // recreate the router w/o the deleted route
-    tarpitRouter.stack = tarpitRouter.stack.filter((layer) => {
-      return !(layer.route && layer.route.path === route);
-    });
+class Pit {
+  constructor() {
+    this.tarpitRouter = express.Router();
+    this.routeHandlers = new Map();
+    this.inited = false;
+    this.count = 0;
   }
-}
+  _rand() {
+      return Math.sqrt(Math.random() * 10) * 500;
+  }
 
-function pit(app, instanceRoot, req) {
-  let newRoute = `/${makeRoute()}/`;
-  count++;
-  console.log(`Created new route, route count is now ${count}`);
+  _makeRoute() {
+    let length = Math.max(Math.floor(Math.random() * 10), 2);
+    let words = [];
+    for (let i = 0; i < length; i++) words.push(randomWord());
+    return words.join("-");
 
-  const handler = (req, res) => {
+  }
+
+  _selfDestruct(route) {
+    if (this.routeHandlers.has(route)) {
+      this.routeHandlers.delete(route)
+
+      // recreate the router w/o the deleted route
+      this.tarpitRouter.stack = this.tarpitRouter.stack.filter((layer) => {
+        return !(layer.route && layer.route.path === route);
+      })
+
+    }
+  }
+  pit(app, instanceRoot) {
+    let newRoute = `/${this._makeRoute()}/`;
+    this.count++;
+    console.log(`Created new route, count : ${this.count}`);
+
+    const handler = (req, res) => {
     // use a promise to avoid a stack overflow
-    Promise.resolve().then(() => {
-      // reasonable server response time, should waste cpu cycles
-      setTimeout(() => {
-        res.send(tar(pit(app, instanceRoot, req), instanceRoot));
-      }, rand() * 0.5);
+      Promise.resolve().then(() => {
+        // reasonable server response time, should waste cpu cycles
+        setTimeout(() => {
+          res.send(tar(this.pit(app, instanceRoot), instanceRoot));
+        }, 0);
 
-      // prevent memory leak by cleaning up old routes : add delay on garbage collection
-      setTimeout(() => {
-        selfDestruct(newRoute);
-      }, rand() * 2);
-    });
-  };
+        // prevent memory leak by cleaning up old routes : add delay on garbage collection
+        setTimeout(() => {
+          this._selfDestruct(newRoute);
+        }, 0);
+      });
+    };
 
-  routeHandlers.set(newRoute, handler);
-  tarpitRouter.get(newRoute, handler);
-  // FOR TESTIng PURPOSES IF THIS MAKES IT TO PROD JUTS SHOOT ME
-  //fetch(`http://localhost:8080/${instanceRoot}${newRoute}`)
+    this.routeHandlers.set(newRoute, handler);
+    this.tarpitRouter.get(newRoute, handler);
 
-  // just so it doesnt get attached multiple times
-  if (!inited) {
-    app.use(tarpitRouter);
-    inited = true;
+    if (!this.inited) {
+      app.use(this.tarpitRouter);
+      this.inited = true;
+    }
+    return newRoute;
+
   }
-  return newRoute;
 }
 
-export { pit };
+export { Pit };
--- src/venus.js
+++ src/venus.js
@@ -1,5 +1,5 @@
 import { venusRoot } from "./lib/venusRoot.js";
-import { pit } from "./lib/tarpit/pit.js";
+import { Pit } from "./lib/tarpit/pit.js";
 import { tar } from "./lib/tarpit/tar.js";
 import express from "express";
 
@@ -7,12 +7,13 @@ class Venus {
   constructor(root = "UNSET") {
     this.root = new venusRoot(root);
     this.prefix = `/${this.root.path}/`;
+    this.pit = new Pit();
   }
   route() {
     const router = express.Router({ mergeParams: true });
     console.log("path: " + `/${this.root.path}/`);
     router.get("/", (req, res) => {
-      let firsturl = pit(router, this.root.path, req); // this will start the recursive hell known as a tarpit
+      let firsturl = this.pit.pit(router, this.root.path); // this will start the recursive hell known as a tarpit
       res.send(tar(firsturl, this.root.path));
       console.log(
         `Creating tarpit for:\nuser-agent- ${req.headers["user-agent"]}`,
